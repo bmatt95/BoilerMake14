c44db7a5d057667b626e6a3fccc407bac84489cb
bmatt95 <mess@purdue.edu>
2014-10-18 15:57:22 -0400

    Update node_server.js
    Update to play with stockLookup

--- node_server.js
+++ node_server.js
@@ -1,24 +1,24 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var exec = require('child_process').execFile
 
 var server = http.createServer(function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     var queryObject = url.parse(req.url,true).query;
     console.log(queryObject);
-    fs.unlink('tick', function(err) {
-        if(err) {
-            console.log(err);
-        }
-    });
-    fs.writeFile('tick', queryObject.symbol, function(err) {
+    fs.writeFile('ticker.txt', queryObject.symbol, function(err) {
         if(err) {
             console.log(err);
         } else {
             console.log("ok!");
         }
     });
-    fs.readFile('test', function (err, data) {
+    exec('InfoGetterBat.bat', function(err, data) {
+        console.log(err)
+        console.log(data.toString());
+    });
+    fs.readFile('price.txt', function (err, data) {
         if (err) throw err;
     res.end(JSON.stringify({"Data":{"LastPrice":parseFloat(data)}}));
     });
