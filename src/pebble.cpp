63fd4af1785a5a17c785d9591e160f0b9355db24
Nick Lamping <NickLamping@users.noreply.github.com>
2014-10-18 16:40:42 -0400

    Update pebble.cpp

--- src/pebble.cpp
+++ src/pebble.cpp
@@ -2,32 +2,6 @@
 
 #include "entry.h"
 
-static void init(void) {
-  wasFirstMsg = false;
-  dataInited = false;
-  refreshKey = -1;
-  refreshSymbol = NULL;
-
-  app_message_init();
-
-  char entry_title[] = "Enter Symbol";
-  entry_init(entry_title);
-
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-
-  const bool animated = true;
-  window_stack_push(window, animated);
-}
-
-static void deinit(void) {
-  window_destroy(window);
-}
-
 int main(void) {
   init();
   deinit();
