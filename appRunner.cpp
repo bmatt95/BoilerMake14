6fd39efc92dac029f9f5840a59c3b1f070350897
Nick Lamping <NickLamping@users.noreply.github.com>
2014-10-18 02:13:28 -0400

    Create appRunner.cpp

---
+++ appRunner.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+/*
+* An application for the Pebble smartwatch that tracks stock info, 
+* using the Bloomberg API, the user inputs so that the wearer of the watch
+* can track stocks away from their phones or personal computers
+*/
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
